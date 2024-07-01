bb07b80a3abd6a4e54007bcfe5e84741f4d9625d
Gao Shenghan <gsh1130@sjtu.edu.cn>
2024-07-01 23:03:51 +0800

    a ball on the curve

--- Math285_Project.m
+++ Math285_Project.m
@@ -1,14 +1,39 @@
 % Parameters
-r = 1;  % Radius of the rolling circle
-theta = linspace(0, 2*pi, 1000);  % Angle parameter
-x = r * (theta - sin(theta));  % x-coordinate of cycloid
-y = -r * (1 - cos(theta));  % y-coordinate of cycloid
+r = 1; % Radius of the generating circle
+g = 9.81; % Acceleration due to gravity
 
-% Plotting
+% Time properties
+T = 2 * pi * sqrt(r / g); % Time for one complete period
+t = linspace(0, T, 1000); % Time vector
+
+% Cycloid Parametric Equations
+theta = (t / T) * 2 * pi; % Parametric variable
+x = r * (theta - sin(theta)); % x coordinate
+y = r * (1 - cos(theta)); % y coordinate
+
+% Plot Cycloid
 figure;
-plot(x, y, 'b', 'LineWidth', 2);
+plot(x, y, 'b-', 'LineWidth', 2);
+hold on;
 axis equal;
-title('Cycloid Curve (Tautochrone Problem)');
+grid on;
 xlabel('x');
 ylabel('y');
-grid on;
+title('Cycloid - Tautochrone Curve');
+set(gca, 'YDir', 'reverse');
+
+% Animate the Ball on the Cycloid
+ball_radius = 0.05;
+for k = 1:length(t)
+    % Clear previous ball
+    if k > 1
+        delete(ball);
+    end
+    
+    % Plot Ball
+    ball = rectangle('Position', [x(k) - ball_radius, y(k) - ball_radius, 2 * ball_radius, 2 * ball_radius], ...
+                     'Curvature', [1, 1], 'FaceColor', 'r');
+    
+    % Pause for animation
+    pause(0.01);
+end
